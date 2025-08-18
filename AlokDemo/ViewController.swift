8a97da2b6bfea0000e231d7623cbc26ecabf2bb0
Alok Singh <aloks0583@gmail.com>
2025-08-18 19:08:40 +0530

    Initial Commit

---
+++ AlokDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AlokDemo
+//
+//  Created by Alok SIngh on 18/08/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ AlokDemoTests/AlokDemoTests.swift
@@ -0,0 +1,17 @@
+//
+//  AlokDemoTests.swift
+//  AlokDemoTests
+//
+//  Created by Alok SIngh on 18/08/25.
+//
+
+import Testing
+@testable import AlokDemo
+
+struct AlokDemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
